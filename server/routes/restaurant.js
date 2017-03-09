08c532e402fbed1cd98092d95977c6be6bd8ea76
Eri Irawan <email": null>
2017-03-09 10:51:28 +0700

    route for restaurant

---
+++ server/routes/restaurant.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/restaurant')
+
+router.get('/', controller.readRest)
+router.post('/create', controller.createRest)
+router.put('/update/:id', controller.updateRest)
+router.delete('/delete/:id', controller.removeRest)
+
+module.exports = router
